a8cd15b66ae89c970be10801916d2b861958ae3e
Jed Mao <jedmao@outlook.com>
2016-02-12 13:30:58 -0600

    Finish tests for #cursorLeft and #cursorRight

--- test/TabSanity.test.ts
+++ test/TabSanity.test.ts
@@ -1,7 +1,8 @@
 import { EOL } from 'os';
 import * as assert from 'assert';
 import {
-	Position
+	Position,
+	Selection
 } from 'vscode';
 
 import TabSanity from '../src/TabSanity';
@@ -13,24 +14,63 @@ import {
 
 suite("TabSanity Tests", () => {
 
+	const ts = createTabSanityFromLines([
+		'    foo  ',
+		'  bar    ',
+		'          baz    qux '
+	]);
+	const expectedPositions = [
+		[0, 4, 5, 6, 7, 8, 9],
+		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+		[0, 4, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+	];
+
 	test("#cursorRight", () => {
-		const ts = createTabSanityFromLines([
-			'    foo  ',
-			'  bar    ',
-			'          baz '
-		]);
-		const expectedPositions = [
-			[4, 5, 6, 7, 8, 9],
-			[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-			[0, 4, 8, 9, 10, 11, 12, 13, 14]
-		];
-		for (const positions of expectedPositions) {
-			for (const expected of positions) {
+		selectBeginningOfDocument();
+		for (let i = 0; i < expectedPositions.length; i++) {
+			const positions = expectedPositions[i];
+			for (let j = 0; j < positions.length; j++) {
+				if (i === 0 && j === 0) {
+					continue;
+				}
+				const expected = positions[j];
 				const actual = ts.cursorRight().character;
 				assert.strictEqual(actual, expected);
 			}
 		}
 	});
+
+	test("#cursorLeft", () => {
+		selectEndOfDocument();
+		for (let i = expectedPositions.length - 1; i >= 0; i--) {
+			const positions = expectedPositions[i];
+			for (let j = positions.length - 1; j >= 0; j--) {
+				if (
+					i === expectedPositions.length - 1
+					&& j === positions.length - 1
+				) {
+					continue;
+				}
+				const expected = positions[j];
+				const actual = ts.cursorLeft().character;
+				assert.strictEqual(actual, expected);
+			}
+		}
+	});
+
+	function selectBeginningOfDocument() {
+		const docStart = new Position(0, 0);
+		ts.editor.selections = [new Selection(docStart, docStart)];
+	}
+
+	function selectEndOfDocument() {
+		const lastLine = ts.doc.lineAt(ts.doc.lineCount - 1);
+		const docEnd = new Position(
+			lastLine.lineNumber,
+			lastLine.text.length
+		);
+		ts.editor.selections = [new Selection(docEnd, docEnd)];
+	}
 
 });
 
